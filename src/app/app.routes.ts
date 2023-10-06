75f727d7f2cacca149d0232e9a3b584a7bc48d64
Felipe <felipe@easyteam.rocks>
2023-10-06 01:18:31 -0300

    feat: create layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const APP_ROUTES: Routes = [
-    {
-      path: 'dashboard',
-      loadComponent: () => 
-        import('./components/dashboard/dashboard.component').then(c => c.DashboardComponent),
-    }
-  ]
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/layout/layout.component').then(c => c.LayoutComponent),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./components/dashboard/dashboard.component').then(c => c.DashboardComponent),
+  },
+]
